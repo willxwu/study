ed94d3a4a4fddc9d586f82d8c890c4cf0cca4a74
Blue <oncwnuD4V7iybvuZHnwf_BbdLNCI@git.weixin.qq.com>
2022-09-05 16:03:29 +0800

    增加java8新特性学习微服务

---
+++ study-java8/src/test/java/com/example/study/java8/StudyJava8ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.study.java8;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StudyJava8ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
