03c1660299b0b7d878966218ecf6378336ff2fbe
Blue <oncwnuD4V7iybvuZHnwf_BbdLNCI@git.weixin.qq.com>
2022-09-06 09:34:37 +0800

    Lambda表达式基本语法 Lambda函数式接口: Functional、Consumer、Predicate、Supplier函数式接口用法详解.

---
+++ study-java8/src/main/java/com/example/study/java8/InterfaceFunction/FunctionalInterfaceDemo.java
@@ -0,0 +1,10 @@
+package com.example.study.java8.InterfaceFunction;
+
+/**
+ * 函数式接口
+ */
+@FunctionalInterface
+public interface FunctionalInterfaceDemo {
+
+    boolean colorFilter(Apple apples);
+}
